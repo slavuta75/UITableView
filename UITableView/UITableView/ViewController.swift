3d04f8e2922794350752ce4ff442bb337eb785c7
Slavuta75 <slavuta75@gmail.com>
2018-06-27 19:36:11 +0300

    beginning

---
+++ UITableView/UITableView/ViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  UITableView
+//
+//  Created by Slava Utyuzh on 27.06.2018.
+//  Copyright © 2018 Slava Utyuzh. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    
+
+}
+
